d877940b4e092fcd9874915be1b2217ba1dd46d4
i.soroczynski <i.soroczynski@techocean.pl>
2022-05-27 22:38:10 +0200

    Fixing bugs related to invalid .imu files; refactor

--- main.c
+++ main.c
@@ -7,7 +7,109 @@
 #include <limits.h>
 
 
-int check_arguments(int argc) 
+int check_arguments(int argc);
+
+int main(int argc, char* argv[])
+{
+    float velocity_min = INT_MAX;
+    float velocity_max = INT_MIN;
+    float velocity_val = 0;
+
+    imu_t imu;
+    FILE *fptr = NULL;
+    tinydir_dir dir;
+    tinydir_file file;
+
+
+    if(check_arguments(argc) != 0)
+    {  
+        return -1;
+    }
+
+    if(tinydir_open(&dir, argv[1]) == -1)
+    {
+        printf("Directory not found!\n");
+        return -1;
+    }
+
+    while(dir.has_next) 
+    {        
+        if(tinydir_readfile(&dir, &file) == -1)
+        {
+            printf("TinyDir File read error!\n");
+            goto error;
+        }
+        
+        // check if file has .imu extension, if no take another one
+        if(strcmp(file.extension, "imu") != 0)
+        {
+            if(tinydir_next(&dir) == -1) {
+                printf("Error during capturing next file.\n");
+                goto error;
+            }
+
+            continue;
+        }
+
+        fptr = fopen((char*)file.path, "rb");
+        if(fptr == NULL)
+        {
+            printf("File open failed!\n");
+            goto error;
+        }
+
+        // Try to read data directly to a struct, if data size is different than struct size skip file 
+        if(fread(&imu, sizeof(char), sizeof(imu), fptr) != sizeof(imu))
+        {
+            printf("Data read fail!\n");
+            fclose(fptr);
+            if(tinydir_next(&dir) == -1) 
+            {
+                printf("Error with getting next file.\n");
+                goto error;
+            }
+
+            continue;
+        }
+
+        // calculate velocity magnitude from three axis
+        velocity_val = sqrtf(powf(imu.velocity[0], 2) + powf(imu.velocity[1], 2) + powf(imu.velocity[2], 2));
+
+        // update min and max values
+        if(velocity_min > velocity_val)
+        {
+            velocity_min = velocity_val;
+        }
+
+        else if(velocity_max < velocity_val)
+        {
+            velocity_max = velocity_val;
+        }
+
+        fclose(fptr);
+
+        // get next file
+        if(tinydir_next(&dir) == -1) {
+            printf("Error with getting next file.\n");
+            goto error;
+        }
+    }
+
+    // if velocity_min and velocity_max remain unchanged there wasn't valid data inside dir
+    if(velocity_min != INT_MAX && velocity_max != INT_MIN)
+    {
+        printf("Min velocity: %f \t Max velocity: %f\n", velocity_min, velocity_max);
+    }
+    else {
+        printf("Coulnd't find any .imu file!\n");
+    }
+
+error:
+    tinydir_close(&dir);
+    return 0;
+}
+
+int check_arguments(int argc)
 {
     if(argc == 2) {
         return 0;
@@ -20,97 +122,6 @@ int check_arguments(int argc)
 
     else {
         printf("Too many arguments!\n");
-        return 1;
-    }
-}
-
-float velocity_min = (float)INT_MAX;
-float velocity_max = (float)INT_MIN;
-
-int main(int argc, char* argv[])
-{
-    imu_t imu;
-    FILE *fptr;
-    tinydir_dir dir;
-
-    if(check_arguments(argc) != 0)
-    {  
         return -1;
     }
-
-    if(tinydir_open(&dir, argv[1]) == -1)
-    {
-        printf("Error with directory path!\n");
-        return -1;
-    }
-
-    while(dir.has_next) 
-    {
-        tinydir_file file;
-        if(tinydir_readfile(&dir, &file) == -1)
-        {
-            printf("Error during file read!\n");
-            goto Tinydir_handler;
-        }
-
-        if(strcmp(file.extension, "imu") != 0)
-        {
-            if(tinydir_next(&dir) == -1) {
-                printf("Error with getting next file.\n");
-                goto Tinydir_handler;
-            }
-
-            continue;
-        }
-
-        // printf("%s\n", file.name); // debug
-        fptr = fopen((char*)file.path, "rb");
-        if(fptr == NULL)
-        {
-            printf("Error during FILE read!\n");
-            goto Tinydir_handler;
-        }
-
-        // read data to imu struct
-        if(fread(&imu, sizeof(char), sizeof(imu), fptr) == 0)
-        {
-            printf("Error during imu data read\n");
-        }
-
-        // printf("Velocity: %f, %f, %f\n", imu.velocity[0], imu.velocity[1], imu.velocity[2]);
-
-        // calculate velocity from three axis
-        float res_velocity;
-        res_velocity = sqrtf(powf(imu.velocity[0], 2) + powf(imu.velocity[1], 2) + powf(imu.velocity[2], 2));
-        // printf("Result velocity: %f\n", res_velocity);
-
-        // update min and max values
-        if(velocity_min > res_velocity)
-        {
-            velocity_min = res_velocity;
-        }
-
-        else if(velocity_max < res_velocity)
-        {
-            velocity_max = res_velocity;
-        }
-
-        // fseek(fptr, 0, SEEK_END); 
-        // size_t file_size_in_bytes = ftell(fptr);
-        // fseek(fptr, 0, SEEK_SET);
-
-        fclose(fptr);
-
-        if(tinydir_next(&dir) == -1) {
-            printf("Error with getting next file.\n");
-            goto Tinydir_handler;
-        }
-    }
-
-    // print results
-    printf("Min velocity: %f \t Max velocity: %f\n", velocity_min, velocity_max);
-
-Tinydir_handler:
-    tinydir_close(&dir);
-    return 0;
 }
